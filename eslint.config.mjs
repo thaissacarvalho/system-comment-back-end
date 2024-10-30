4cfe711b24a8ef3c8af0abb86925d3a4d6b7a0f2
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2024-10-30 11:48:38 -0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    semi: ['error', 'always'], // Adicionando a regra para usar ponto e vírgula
+  },
+};
---
+++ src/infra/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript with Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
