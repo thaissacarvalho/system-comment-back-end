792af5d909d8c41dc26d87615eae1291d184154b
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2024-11-06 19:25:10 -0300

    add cors and helmet

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -1,14 +1,24 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import { router } from '../app/routes/routes';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript with Express!');
-});
+// Configuração do CORS para permitir acessos de um domínio específico
+app.use(cors({
+  origin: 'https://bythaissa.com',  // Permitir apenas este domínio
+  methods: 'GET,POST,PUT,DELETE',          // Permitir esses métodos
+}));
+
+// Usar o Helmet para proteger a aplicação com cabeçalhos de segurança
+app.use(helmet());
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
